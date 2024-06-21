025b23756335eedf0983ef575e599f0054d361b4
Sho0226 <s1F102201616@iniad.org>
2024-06-22 07:17:41 +0900

    add: ball

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,15 @@ import styles from './index.module.css';
 const Home = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [paddleX, setPaddleX] = useState((800 - 75) / 2);
+  const [ballX, setBallX] = useState(400);
+  const [ballY, setBallY] = useState(300);
+  const [ballDX, setBallDX] = useState(2);
+  const [ballDY, setBallDY] = useState(2);
   const paddleWidth = 75;
   const paddleHeight = 10;
   const canvasHeight = 600;
   const canvasWidth = 800;
+  const ballRadius = 10;
 
   useEffect(() => {
     const handlekeyDown = (event: KeyboardEvent) => {
@@ -30,14 +35,64 @@ const Home = () => {
     if (canvas) {
       const ctx = canvas?.getContext('2d');
       if (ctx) {
-        ctx.fillStyle = 'black';
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        const interval = setInterval(() => {
+          setBallX((c) => {
+            let newX = c + ballDX;
+            if (newX > canvasWidth - ballRadius || newX < ballRadius) {
+              setBallDX(-ballDX);
+              newX = c + ballDX;
+            }
+            return newX;
+          });
 
-        ctx.fillStyle = 'white';
-        ctx.fillRect(paddleX, canvasHeight - paddleHeight, paddleWidth, paddleHeight);
+          setBallY((c) => {
+            let newY = c + ballDY;
+            if (newY < ballRadius) {
+              setBallDY(-ballDY);
+              newY = c + ballDY;
+            } else if (newY > canvasHeight - ballRadius) {
+              if (ballX > paddleX && ballX < paddleX + paddleWidth) {
+                setBallDY(-ballDY);
+                newY = c + ballDY;
+              } else {
+                clearInterval(interval);
+                alert('Game Over');
+              }
+            }
+            return newY;
+          });
+
+          ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+          ctx.fillStyle = 'black';
+          ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+          ctx.fillStyle = 'white';
+          ctx.fillRect(paddleX, canvasHeight - paddleHeight, paddleWidth, paddleHeight);
+
+          ctx.beginPath();
+          ctx.arc(ballX, ballY, ballRadius, 0, Math.PI * 2);
+          ctx.fillStyle = 'white';
+          ctx.fill();
+          ctx.closePath();
+        }, 10);
+
+        return () => clearInterval(interval);
       }
     }
-  }, [paddleX]);
+  }, [
+    ballX,
+    ballY,
+    ballDX,
+    ballDY,
+    paddleX,
+    canvasWidth,
+    canvasHeight,
+    paddleWidth,
+    paddleHeight,
+    ballRadius,
+  ]);
+
   return (
     <div className="Home">
       <canvas ref={canvasRef} width={800} height={600} className={styles.canvas} />
