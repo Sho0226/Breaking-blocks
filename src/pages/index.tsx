ab7acfc9043f39bd9f7e2feac1798f0a4dca2ef0
Sho0226 <s1F102201616@iniad.org>
2024-06-22 04:06:26 +0900

    add: canvas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
+import { useEffect, useRef } from 'react';
 import styles from './index.module.css';
 
 const Home = () => {
-  return <div className={styles.container} />;
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (canvas) {
+      const ctx = canvas?.getContext('2d');
+      if (ctx) {
+        ctx.fillStyle = 'black';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+      }
+    }
+  }, []);
+  return (
+    <div className="Home">
+      <canvas ref={canvasRef} width={800} height={600} className={styles.canvas} />
+    </div>
+  );
 };
 
 export default Home;
