b6de86e8b500cdfd264965ee6609160032018e7d
Sho0226 <s1F102201616@iniad.org>
2024-07-23 11:25:42 +0900

    fix: favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <title>Breaking-blocks </title>
         <meta name="description" content="Breaking-blocks " />
-        <link rel="icon" href="favicon.ico" />
+        <link rel="icon" href="favicon.png" />
       </Head>
       <body>
         <Main />
